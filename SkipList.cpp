68f4f87656770ef6dd1ad3efd5f144103fa23d2b
elkorn <helluinster@gmail.com>
2014-01-13 21:20:53 +0100

    [skip-list] Adding removal functionality.

--- SkipList.cpp
+++ SkipList.cpp
@@ -37,13 +37,93 @@ SkipList<Key, Val>::SkipList(float theProbability, int theMaxHeight, Key* theKey
 
 template <class Key, class Val>
 bool SkipList<Key, Val>::insert(Key* theKey, Val* theValue) {
-    int lvl = 0, h = currentHeight;
+    int level = 0, h = currentHeight;
     SkipListNode<Key, Val>** toUpdate = new SkipListNode<Key, Val>*[maxHeight+1];
     SkipListNode<Key, Val>* tempNode = head;
     Key* compareKey;
+    // Check all the height levels for where to insert the new node.
     for(; h > 0; --h) {
-        
+        compareKey = tempNode->next[h]->getKey();
+        while(*compareKey < * *theKey) {
+            // We have to go further.
+            tempNode = tempNode->next[h];
+            compareKey = tempNode->next[h]->getKey();
+        }
+
+        // This node at level h will have to be updated after inserting the new one.
+        toUpdate[h] = tempNode;
     }
+
+    tempNode = tempNode->next[1];
+    compareKey = tempNode->getKey();
+
+    if(*compareKey == *theKey) {
+        // We already have this key in the set - cannot insert.
+        return false;
+    }
+
+    // Get a random level for the node to be inserted into.
+    level = randomizer->newLevel();
+    if(level > currentHeight) {
+        // Create all the levels between the previous and new currentHeight
+        // and add them to the update matrix.
+        for(int i = currentHeight + 1; i <= level; ++i) toUpdate[i] = head; // The default lookup node.
+        currentHeight = level;
+    }
+
+    tempNode = new SkipListNode<Key, Val>(theKey, theValue, level);
+
+    // Actually insert the node where it belongs.
+    for (int i = 1; i <= level; ++i) {
+        tempNode->next[i] = toUpdate[i]->next[i];
+        toUpdate[i]->next[i] = tempNode;
+    }
+
+    // Success!
+    return true;
+}
+
+template <class Key, class Val>
+bool SkipList<Key, Val>::remove(Key* theKey) {
+    SkipListNode<Key, Val>** toUpdate = new SkipListNode<Key, Val>*[maxHeight+1];
+    SkipListNode<Key,Val>* tempNode = head;
+    Key* compareKey;
+
+    for (int h = currentHeight; h > 0; --h) {
+        compareKey = tempNode->next[h]->getKey();
+        while(*compareKey < *theKey) {
+            tempNode = tempNode->next[h];
+            compareKey = tempNode->next[h]->getKey();
+        }
+
+        toUpdate[h] = tempNode;
+    }
+
+    // Get the found node at the base level.
+    tempNode = tempNode->next[1];
+    compareKey = tempNode->getKey();
+
+    if (*compareKey == *theKey) {
+        for (int i = 1; i <= currentHeight; ++i) {
+            if (toUpdate[i]->next[i] != tempNode) break; // The removed node does not exist at this level.
+            toUpdate[i]->next[i] = tempNode->next[i]; // Wire up the pointers.
+        }
+
+        delete tempNode;
+
+        // Adjust currentHeight.
+        while ((currentHeight > 1) &&
+            (head->next[currentHeight]->getKey() == tail->getKey())) {
+            // If there are no nodes at currentHeight, decrement the value - no need to poke around there.
+            --currentHeight;
+        }
+
+        // Success!
+        return true;
+    }
+
+    // Node not found - nothing to remove.
+    return false;
 }
 
 template <class Key, class Val>
