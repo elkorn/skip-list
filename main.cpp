b5f7678341684ed450026afde477dd91b3083975
elkorn <helluinster@gmail.com>
2014-01-18 22:07:37 +0100

    [skip-list] Adding a bit more test code.

--- main.cpp
+++ main.cpp
@@ -10,14 +10,18 @@ int main(int argc, char** argv) {
     int testVal1 = 12, testVal2 = 1238, testVal3 = 124546;
     SkipList<string, int> *theList = new SkipList<string, int>((float).6, 5, maxKey);
     theList->insert("a", &testVal1);
-    theList->insert("c", &testVal1);
+    theList->insert("c", &testVal2);
     theList->print(cout);
 
-    cout << theList->find("c");
+    cout << theList->find("c") << endl;
     try {
         cout << theList->find("xxyzzyxx");
     } catch(ElementNotFoundException<string> &ex) {
         cout << ex.what();
     }
+
+    theList->insert("b", &testVal3);
+
+    theList->print(cout);
     return 0;
 }
